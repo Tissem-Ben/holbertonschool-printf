f98d3ae87ddb945688980b30310fa8e51f8fa8cb
oussamafrt <oussama.ferhat01@gmail.com>
2024-07-22 19:00:40 +0200

    main file for the printf projetc

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+void get_conv_func(char a, va_list args, int *count);
+void print_char (va_list args, int *count);
+void print_string (va_list args, int *count);
+void print_pourcentage (va_list args, int *count);
+typedef struct specifier
+{
+	char *specifier;
+	void (*f)(va_list args, int *count);
+} specifier_option;
+#endif
