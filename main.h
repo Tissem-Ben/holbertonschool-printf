87ecd29506c6f54ff523597ad98d557181f5438c
Tissem-Ben <t.bendahma@gmail.com>
2024-07-24 10:47:49 +0200

    main.h

--- main.h
+++ main.h
@@ -1,8 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
 int _printf(const char *format, ...);
-
-#endif /* MAIN.H */ 
+void get_conv_func(char directive, va_list args, int *count);
+void print_char (va_list args, int *count);
+void print_string (va_list args, int *count);
+void print_pourcentage (va_list args, int *count);
+typedef struct specifier_struct
+{
+		char *specifier;
+			void (*f)(va_list args, int *count);
+} specifier_option;
+#endif
