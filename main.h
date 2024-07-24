b1f04b83d833e648c652cd9ca0fca014f8f6efad
oussamafrt <oussama.ferhat01@gmail.com>
2024-07-24 14:22:19 +0200

    printf project

--- main.h
+++ main.h
@@ -10,6 +10,7 @@ void get_conv_func(char directive, va_list args, int *count);
 void print_char (va_list args, int *count);
 void print_string (va_list args, int *count);
 void print_pourcentage (va_list args, int *count);
+void print_integer(va_list args, int *count);
 typedef struct specifier_struct
 {
 	char *specifier;
